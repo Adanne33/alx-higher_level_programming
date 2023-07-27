2311a359bf0bbea013024b43e857033e1ec6804d
adanne33 <adannentobari@gmail.com>
2023-07-27 21:19:53 +0000

    reads and prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const argv = process.argv;
 const filePath = argv[2];
-const fd = require('fx');
+const fd = require('fs');
 fd.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) { console.log(err); } else { console.log(data); }
+  if (err) { console.log(err); } else { process.stdout.write(data); }
 });
